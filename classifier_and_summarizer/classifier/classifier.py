5ee212916c38560ef04c6328a575bd92aed8e14b
Vignesh S <45061877+telescopic@users.noreply.github.com>
2020-06-22 12:11:51 +0530

    Added basic classifier (#38)

---
+++ classifier_and_summarizer/classifier/classifier.py
@@ -0,0 +1,35 @@
+class Classifier:
+    '''Class to determine if a webpage is stampable
+
+    Detemines if a webpage is stampable based on the following
+    max(media_count,text_count) + embedded_content_count >= min_pages
+    '''
+
+    def __init__(
+            self,
+            normal_text_contents,
+            title_text_contents,
+            media_contents,
+            embedded_contents,
+            max_pages):
+        self.normal_text_count = len(normal_text_contents)
+        self.title_text_count = len(title_text_contents)
+        self.media_count = len(media_contents)
+        self.embedded_content_count = len(embedded_contents)
+        # max pages is maximum number of stamp pages allowed
+        # min pages is the minimum number of stamp pages
+        # required
+        self.min_pages = max_pages // 2
+
+    def classify(self):
+        # max is picked since some unused media/sentences
+        # might still be used for stamp page contents
+        self.is_stampifiable \
+            = max(
+                max(self.normal_text_count, self.title_text_count),
+                self.media_count) \
+            + self.embedded_content_count >= self.min_pages
+
+    def is_page_stampifiable(self):
+        self.classify()
+        return self.is_stampifiable
