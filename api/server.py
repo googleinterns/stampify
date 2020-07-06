a8794f32a53aaa3de7a9e1dbb20a62f8730aa191
Deepit <deepitpatel@google.com>
2020-07-06 17:28:25 +0530

    Add docker

--- api/server.py
+++ api/server.py
@@ -57,4 +57,4 @@ def show_stamp():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0",port=5000)
--- run_server.py
+++ run_server.py
@@ -1,3 +1,4 @@
 from api.server import app
 
-app.run()
+if __name__ == '__main__':
+  app.run()
