9b29e559a3e2c325a42cac9ff915dea07f491651
Vignesh S <45061877+telescopic@users.noreply.github.com>
2020-07-06 17:16:45 +0530

    Added functionality to separate title text (#105)
    * Added functionality to separate title text

--- data_models/text.py
+++ data_models/text.py
@@ -5,6 +5,9 @@ from data_models.contents import ContentType, _Content
 
 class Text(_Content):
     """This class creates Text object"""
+    TITLE_CONTENT_TYPES = [
+        "h1", "h2", "h3", "h4", "h5", "h6"
+    ]
 
     def __init__(self, text_str, text_type='', is_bold=False):
         super(Text, self).__init__(ContentType.TEXT)
@@ -12,3 +15,7 @@ class Text(_Content):
         self.text_string = text_str
         self.font_style = ''
         self.is_bold = is_bold
+
+    def is_important_text(self):
+        return self.content_type in self.TITLE_CONTENT_TYPES \
+            or self.is_bold
